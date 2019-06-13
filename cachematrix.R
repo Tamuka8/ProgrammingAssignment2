509e24a3688a7af7ead86d6d8998753bc205aa35
Tamuka M <t_mazana@hotmail.com>
2019-06-13 01:42:04 -0500

    Update Chache.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R codes to calculate the invert 3 by 3 matrix
+rm(list=ls())
+## function for matrix entry
 
-## Write a short comment describing this function
-
+## function for matrix entry
 makeCacheMatrix <- function(x = matrix()) {
-
+h=matrix(c(a:b), nrow=c, ncol=d)
+        return(h)
 }
 
 
-## Write a short comment describing this function
+## function to calculate the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        g = makeCacheMatrix(x)
+        m<-solve(t(g)*g)
+        return(m)
 }
+
+#choose any square matrices
+a=1;b=4;c=2;d=2     #2 by 2 matrices
+a=1;b=9;c=3;d=3     #3 by 3 matrices
+
+makeCacheMatrix(x)
+cacheSolve(x)
